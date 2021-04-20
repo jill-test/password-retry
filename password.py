8a64f1f5675d41d0301a4cdfa356dd0b8973aefb
Jill Lin <jill0.0test@gmail.com>
2021-04-20 18:39:40 +0800

    update

--- password.py
+++ password.py
@@ -7,11 +7,13 @@
 pwd1 = 'a123456'
 y = 3
 while y > 0:
-	pwd2 = input('請輸入密碼: ')	
 	y = y - 1
+	pwd2 = input('請輸入密碼: ')		
 	if pwd2 == pwd1:
 		print('登入成功!')
 		break
 	else:
-		print('密碼錯誤! 還有', y, '次機會')
-		
+		if y > 0:		
+			print('密碼錯誤! 還有', y, '次機會')
+		else:
+			print('密碼錯誤! 沒有機會')			
